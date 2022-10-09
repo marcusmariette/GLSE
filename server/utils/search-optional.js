0094030ccd9306fd884533e0c55074d7c7096ba3
Edrlc <edricloh88@gmail.com>
2022-10-10 09:37:28 +1100

    [Edric]-[Fix bug when using all three symbols]

--- server/utils/search-optional.js
+++ server/utils/search-optional.js
@@ -1,24 +1,42 @@
 function searchWithOptional(query, results) {
     let newResults = [];
     let matchOptional;
+    let matched = query.match(/\?\w+/g)
     if (results.length === 0) {
-        matchOptional = query.match(/\?\w+/g);
+        matchOptional = matched;
     } else {
         matchOptional = results.map((match) => match.match(/\?\w+/g));
     }
-    if (matchOptional != null) {
-        matchOptional.forEach((string, index) => {
-            let wordWithSelectedOptional = query.replace(matchOptional[index], "").replace("?", "");
-            newResults.push(removeWhiteSpaceFrom(wordWithSelectedOptional))
-            if (matchOptional.length - 1 === index) {
-                let wordWithOptionals = query.replaceAll("?", "").trim()
-                newResults.push(wordWithOptionals)
-                let wordWithoutOptionals = query.replaceAll(/\?\w+/g, "")
-                newResults.push(removeWhiteSpaceFrom(wordWithoutOptionals))
-            }
-        });
+    if (matchOptional != null && matched != null) {
+        if (results.length === 0) {
+            matchOptional.forEach((string, index) => {
+                let wordWithSelectedOptional = query.replace(matchOptional[index], "").replace("?", "");
+                newResults.push(removeWhiteSpaceFrom(wordWithSelectedOptional))
+                if (matchOptional.length - 1 === index) {
+                    let wordWithOptionals = query.replaceAll("?", "").trim()
+                    newResults.push(wordWithOptionals)
+                    let wordWithoutOptionals = query.replaceAll(/\?\w+/g, "")
+                    newResults.push(removeWhiteSpaceFrom(wordWithoutOptionals))
+                }
+            });
+        } else {
+            results.forEach(result => {
+                matchOptional.forEach((string, index) => {
+                    let wordWithSelectedOptional = result.replace(matchOptional[index], "").replace("?", "");
+                    newResults.push(removeWhiteSpaceFrom(wordWithSelectedOptional))
+                    if (matchOptional.length - 1 === index) {
+                        let wordWithOptionals = result.replaceAll("?", "").trim()
+                        newResults.push(wordWithOptionals)
+                        let wordWithoutOptionals = result.replaceAll(/\?\w+/g, "")
+                        newResults.push(removeWhiteSpaceFrom(wordWithoutOptionals))
+                    }
+                });
+            })
+        }
+        return newResults;
+    } else {
+        return results;
     }
-    return newResults;
 }
 
 function removeWhiteSpaceFrom(string) {
